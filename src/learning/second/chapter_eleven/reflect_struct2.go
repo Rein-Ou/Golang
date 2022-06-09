b8fd9639e41d0bfcb3c5f5c79ae36b9315780c75
Rein <390041412@qq.com>
2022-06-09 16:28:04 +0800

    chapter 11

--- src/learning/second/chapter_eleven/reflect_struct2.go
+++ src/learning/second/chapter_eleven/reflect_struct2.go
@@ -16,7 +16,8 @@ func main() {
 	typeOfT := s.Type()
 	for i := 0; i < s.NumField(); i++ {
 		f := s.Field(i)
-		fmt.Printf("%d: %s %s= %v\n", i, typeOfT.Field(i).Name, f.Type(), f.Interface())
+		fmt.Printf("%d: %s %s= %v\n", i,
+			typeOfT.Field(i).Name, f.Type(), f.Interface())
 	}
 	s.Field(0).SetInt(77)
 	s.Field(1).SetString("Sunset Strip")
